be0899e76e8eca788c9047791324359cee4f2220
Dennis Lindquist <dennislindquist@gmail.com>
2021-07-05 14:34:33 +0200

    La till Button och Entrys

---
+++ PayMe/PayMe/ViewModels/AddPageViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace PayMe.ViewModels
+{
+    public class AddPageViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+
+        public AddPageViewModel()
+        {
+
+        }
+    }
+}
